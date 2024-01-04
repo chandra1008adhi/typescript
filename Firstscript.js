60673be27989b8b22eb961450c0cd06591bf6ad2
Adhichandu1008 <adhimulam.chandra1008@gmail.com>
2024-01-04 12:30:04 +0530

    first program

---
+++ Firstscript.js
@@ -0,0 +1,5 @@
+function add1(a, b) {
+    return a + b;
+}
+var sum = add1(30, 20);
+console.log("sum of numbers " + sum);
---
+++ Firstscript.ts
@@ -0,0 +1,5 @@
+function add1(a:number, b:number){
+    return a+b;
+}
+let sum:number=add1(30,20);
+console.log("sum of numbers "+sum);
